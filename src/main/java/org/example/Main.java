e2b4eb6f85f8657905b4b322dda6150e66f23d8f
user <akromjonovdoniyor13@gmail.com>
2025-05-14 14:48:31 +0500

    Add

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,6 +11,7 @@ public class Main {
             MyBotService service = new MyBotService();
             telegramBotsApi.registerBot(new MyBot(service));
             System.out.println("Telegram bot is running");
+            System.out.println("Hello world");
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);
         }
